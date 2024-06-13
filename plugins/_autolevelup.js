b13ec0b4444cad7db051513953277581eb6fff1a
Aurtherle <113518431+Aurtherle@users.noreply.github.com>
2024-06-13 22:28:40 +0300

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,14 +1,14 @@
 //import db from '../lib/database.js'
-import { canLevelUp } from '../lib/levelling.js'
+//import { canLevelUp } from '../lib/levelling.js'
 
-export async function before(m, { conn }) {
-    let user = global.db.data.users[m.sender]
-    if (!user.autolevelup)
+//export async function before(m, { conn }) {
+   // let user = global.db.data.users[m.sender]
+  //  if (!user.autolevelup)
         return !0
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier))
+  //  let before = user.level * 1
+ //   while (canLevelUp(user.level, user.exp, global.multiplier))
         user.level++
-    user.role = global.rpg.role(user.level).name
+  //  user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
         m.reply(`*❃ ──────⊰ ❀ ⊱────── ❃*\n
 *🎊ازداد مستواك🎉*
